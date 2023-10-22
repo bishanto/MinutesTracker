e7facfb1a3e8755a93e176b78b833133728a4508
jesuscuevas <jesusacuevas@csus.edu>
2023-10-22 16:13:29 -0700

    DT-42

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,7 +1,23 @@
+import './styleSheet.css';
+
 export const LoginPage = () => {
     return(
-        <form>
-            <h1>Login to Minutes Tracker</h1>
-        </form>
+        <div class="outer-container">
+            <div class="middle-container">
+                <div class="inner-container">
+                    <form>
+                        <h1>Login to Minutes Tracker</h1>
+                        <div>
+                            E-mail:<br/>
+                            <input type="email" placeholder="E-mail" name="email" /><br/><br/>
+                            Password:<br/>
+                            <input type="password" placeholder="Password" name="password" /><br/><br/>
+                        </div>
+                        <br/>
+                        <input type="submit" value="Login"/>
+                    </form>
+                </div>
+            </div>
+        </div>
     )
 }
