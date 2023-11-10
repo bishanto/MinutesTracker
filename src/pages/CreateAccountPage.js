df2ab591351d29095dc7a385303f99a65ac7a6bb
nskhalsa <132542470+nskhalsa@users.noreply.github.com>
2023-11-09 20:41:07 -0800

    Update CreateAccountPage.js
    Added code that accesses database's user table to input account data

--- src/pages/CreateAccountPage.js
+++ src/pages/CreateAccountPage.js
@@ -1,19 +1,57 @@
+import './styleSheet.css';
+import axios from 'axios';
+import React, {useState} from 'react';
+import { useNavigate } from 'react-router-dom';
+
 export const CreateAccountPage = () => {
+
+	const [values,setValues] = useState({
+
+		fname: '',
+		lname: '',
+		email: '',
+		password: ''
+
+	})
+
+	const handleInput = (event) => {
+
+		setValues(prev => ({...prev, [event.target.name]: [event.target.value]}))
+
+	}
+
+	const handleSubmit = (event) => {
+
+		event.preventDefault();
+		axios.post('http://localhost:8081/createaccount', values)
+		.then(res => {
+
+			alert("Account Created");
+			navigate('/login');
+
+		})
+
+		.catch(err => console.log(err))
+		
+	}
+
+	const navigate = useNavigate();
+	
 	return (
 		<div class="outer-container">
 			<div class="middle-container">
-				<form>
+				<form onSubmit={handleSubmit}>
 					<div class="inner-container">
 						<h1>Create a Minutes Tracker Account</h1>
 						<div>
 							First Name<br/>
-							<input type="text" name="firstname" placeholder="First Name" /><br/>
+							<input type="text" name="firstname" placeholder="First Name" onChange={handleInput}/><br/>
 							Last Name<br/>
-							<input type="text" name="lastname" placeholder="Last Name"/><br/>
+							<input type="text" name="lastname" placeholder="Last Name" onChange={handleInput}/><br/>
 							E-mail<br/>
-							<input type="email" name="email" placeholder="E-mail"/><br/>
+							<input type="email" name="email" placeholder="E-mail" onChange={handleInput}/><br/>
 							Create Password<br/>
-							<input type="password" name="password" placeholder="Create Password"/><br/>
+							<input type="password" name="password" placeholder="Create Password" onChange={handleInput}/><br/>
 							Confirm Password<br/>
 							<input type="password" name="password_confirm" placeholder="Confirm Password"/><br/>
 						</div>
