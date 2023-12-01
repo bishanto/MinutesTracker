69c2b711e8d95a4ef6cb4c2f62cf998198b68653
AngeloJay <angelo.jay@outlook.com>
2023-12-01 15:36:19 -0800

    DT-9: Add Kid Profile Page Added a submitable form for adding profile for kids.

--- src/pages/AddProfilePage.js
+++ src/pages/AddProfilePage.js
@@ -1,10 +1,128 @@
-import Navbar from "../components/Navbar/Navbar";
+import "./styleSheet.css";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const AddProfilePage = () => {
+  const [data, setData] = useState([]);
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    teacher: "",
+    grade: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setData((prevData) => [...prevData, formData]);
+    alert("A profile has been added successfully!");
+    navigate("/addprofile");
+
+    setFormData({
+      firstName: "",
+      lastName: "",
+      teacher: "",
+      grade: "",
+    });
+  };
+
+  const tableData = [
+    {
+      TeacherLastName: "Portis",
+    },
+    {
+      TeacherLastName: "Miller",
+    },
+    {
+      TeacherLastName: "Thompson",
+    },
+    {
+      TeacherLastName: "Fox",
+    },
+  ];
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  /* Disables confirm button until all fields are filled */
+  const isFieldFull = () => {
+    return Object.values(formData).some((value) => value === "");
+  };
+  
+  const cancel = () => {
+    navigate("/welcome");
+  };
+
+  const navigate = useNavigate();
+
   return (
-    <form>
-      <Navbar />
-      <h1>Add Profile</h1>
-    </form>
+    <div class="outer-container">
+      <div class="middle-container">
+        <div class="inner-container">
+          <form onSubmit={handleSubmit}>
+            <h1>Add A New Kid's Profile</h1>
+            <div>
+              First Name
+              <br />
+              <input
+                type="text"
+                placeholder="First Name"
+                name="firstName"
+                onChange={handleChange}
+                value={formData.firstName}
+              />
+              <br />
+              Last Name
+              <br />
+              <input
+                type="text"
+                placeholder="Last Name"
+                name="lastName"
+                onChange={handleChange}
+                value={formData.lastName}
+              />
+              <br />
+              Grade
+              <br />
+              <input
+                type="number"
+                min={0}
+                max={12}
+                name="grade"
+                placeholder="Grade"
+                value={formData.grade}
+                onChange={handleChange}
+              />
+              <br />
+              Classroom Teacher
+              <br />
+              <select
+                id="dropdown"
+                name="teacher"
+                value={formData.teacher}
+                onChange={handleChange}
+              >
+                <option value="" hidden>
+                  -- Select A Teacher--
+                </option>
+                {/* Create  a dropdown for every teacher in database */}
+                {tableData.map((data, index) => (
+                  <option key={index}>{data.TeacherLastName}</option>
+                ))}
+              </select>
+            </div>
+            <br />
+            <br />
+            <input type="submit" value="Confirm" disabled={isFieldFull()} />
+            &nbsp;&nbsp;
+            <div>
+              <button onClick={cancel}>Cancel</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
   );
 };
