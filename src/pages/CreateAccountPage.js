c25a9935906ab8946470cb50397f822b3b4408f1
Jesus Cuevas <32606020+jesuscuevas@users.noreply.github.com>
2023-12-01 17:25:52 -0800

    DT-50 Add input validation to Create Account page

--- src/pages/CreateAccountPage.js
+++ src/pages/CreateAccountPage.js
@@ -1,42 +1,53 @@
 import './LoginStyle.css';
 import axios from 'axios';
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import { useNavigate } from 'react-router-dom';
 
 export const CreateAccountPage = () => {
 
-	const [values,setValues] = useState({
-
+	const [values, setValues] = useState({
 		fname: '',
 		lname: '',
 		email: '',
-		password: ''
+		password: '',
+		password_confirm: ''
+	});
 
-	})
+	useEffect(() => {
+		if(String(values.password) === String(values.password_confirm)) {
+			document.getElementsByName("password_confirm").forEach(value => {
+				value.style.boxShadow = "none";
+				value.setCustomValidity("");
+			});
+		} else {
+			document.getElementsByName("password_confirm").forEach(value => {
+				value.style.boxShadow = "0 0 5px 1px red";
+				value.setCustomValidity("Passwords don't match");
+			});
+		}
+	}, [values.password, values.password_confirm]);
 
 	const handleInput = (event) => {
-
-		setValues(prev => ({...prev, [event.target.name]: [event.target.value]}))
-
+		setValues(prev => ({...prev, [event.target.name]: [event.target.value]}));
 	}
 
 	const handleSubmit = (event) => {
-
 		event.preventDefault();
-		axios.post('http://localhost:8081/createaccount', values)
-		.then(res => {
-
-			alert("Account Created");
-			navigate('/login');
-
-		})
-
-		.catch(err => console.log(err))
-		
+		if(String(values.password) === String(values.password_confirm)) {
+			axios.post('http://localhost:8081/createaccount', values)
+			.then(res => {
+				alert("Account Created");
+				navigate('/login');
+			}).catch(err => console.log(err));
+		} else {
+			document.getElementsByName("password_confirm").forEach((value) => {
+				value.style.boxShadow = "0 0 5px 1px red";
+			});
+		}
 	}
 
 	const navigate = useNavigate();
-	
+
 	return (
 		<div class="outer-container">
 			<div class="middle-container">
@@ -44,16 +55,11 @@ export const CreateAccountPage = () => {
 					<div class="inner-container">
 						<h1>Create a Minutes Tracker Account</h1>
 						<div>
-							First Name<br/>
-							<input type="text" name="firstname" placeholder="First Name" onChange={handleInput}/><br/>
-							Last Name<br/>
-							<input type="text" name="lastname" placeholder="Last Name" onChange={handleInput}/><br/>
-							E-mail<br/>
-							<input type="email" name="email" placeholder="E-mail" onChange={handleInput}/><br/>
-							Create Password<br/>
-							<input type="password" name="password" placeholder="Create Password" onChange={handleInput}/><br/>
-							Confirm Password<br/>
-							<input type="password" name="password_confirm" placeholder="Confirm Password"/><br/>
+							First Name<br/><input type="text" name="firstname" placeholder="First Name" onChange={handleInput}/><br/>
+							Last Name<br/><input type="text" name="lastname" placeholder="Last Name" onChange={handleInput}/><br/>
+							E-mail<br/><input type="email" name="email" placeholder="E-mail" onChange={handleInput}/><br/>
+							Create Password<br/><input class="password" type="password" name="password" placeholder="Create Password" onChange={handleInput}/><br/>
+							Confirm Password<br/><input class="password" type="password" name="password_confirm" placeholder="Confirm Password" onChange={handleInput}/><br/>
 						</div>
 						<input type="checkbox" name="checkbox" required />
 						<label for="checkbox">I agree with the <a href="/">Safety Policies</a></label><br/><br/>
