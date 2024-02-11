eaa55014324b6fe7d94917b6d2723e6e55bee495
ncrespi83 <nathancrespi@csus.edu>
2024-02-10 21:33:51 -0800

    added sample data to DragonStatisticsPage

--- src/pages/DragonStatisticsPage.js
+++ src/pages/DragonStatisticsPage.js
@@ -1,4 +1,21 @@
 import Navbar from "../components/Navbar/Navbar";
+mport './styleSheet.css';
+import React, { useEffect, useState } from 'react';
+import Chart from 'chart.js/auto';
+import axios from 'axios';
+
+// Sample data for kid's progress
+const weeklyData = [10, 20, 15, 25, 30];
+const monthlyData = [30, 45, 40, 50, 60];
+const trimesterData = [80, 100, 90, 120, 130];
+
+const bookTitles = [
+    "Book 1",
+    "Book 2",
+    "Book 3",
+    "Book 4",
+    "Book 5"
+];
 
 export const DragonStatisticsPage = () => {
   return (
