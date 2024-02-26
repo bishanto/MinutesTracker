e5ad8412bf017fd142ba0ef1b58b20933cbb039d
ncrespi83 <nathancrespi@csus.edu>
2024-02-25 21:52:39 -0800

    DT-83 commit

--- src/pages/DragonStatisticsPage.js
+++ src/pages/DragonStatisticsPage.js
@@ -9,14 +9,6 @@ const weeklyData = [10, 20, 15, 25, 30];
 const monthlyData = [30, 45, 40, 50, 60];
 const trimesterData = [80, 100, 90, 120, 130];
 
-const bookTitles = [
-    "Book 1",
-    "Book 2",
-    "Book 3",
-    "Book 4",
-    "Book 5"
-];
-
 export const DragonStatisticsPage = () => {
   const [selectedPeriod, setSelectedPeriod] = useState('week');
 
