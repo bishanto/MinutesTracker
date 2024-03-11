4bab35789f3d449fa7010a9e3da785d64ced4973
nskhalsa <132542470+nskhalsa@users.noreply.github.com>
2024-03-10 21:16:03 -0700

    DT-101 Create a more reponsive UI for smaller screen sizes

--- src/pages/WelcomePage.js
+++ src/pages/WelcomePage.js
@@ -1,16 +1,26 @@
 import "./WelcomePageStyle.css";
+import { Container, Row, Col, Card } from "react-bootstrap"; 
+
 export const WelcomePage = () => {
     return(
-        <form>
-	<img class="dragonimg" src="/images/dragonLogo.png" alt="Dragon_Logo"></img>
-        <h1>Dragon Minutes</h1>
-	<div class = "div1">
-        <h3> <a href='#/signin'>Sign-in</a> </h3>
-	</div>
-	<div class ="div2">
-        <h3> <a href='#/createaccount'>Create An Account</a> </h3>
-	</div>
-	<h3> <a href='#/ForgetPassword'>Forget Password</a> </h3>
-    </form>
+        <Container fluid style={{paddingLeft: 0, paddingRight: 0}}> 
+			<Row className="justify-content-center"> 
+				<Col xs={12} md={6} lg={4}>
+					<a href="/"><img class="dragonimg" src="/images/dragonLogo.png" alt="Dragon_Logo"></img></a>
+					<Card style={{borderRadius:30, marginTop: 15}}>
+						<Card.Body>
+							<form>
+							<Card.Title style={{fontSize:40}}>Dragon Minutes</Card.Title> 
+							<Card.Text> 
+								<br/><h3> <a href='#/signin'>Sign-in</a> </h3><br/>
+								<h3> <a href='#/createaccount'>Create An Account</a> </h3><br/>
+								<h3> <a href='#/ForgetPassword'>Forget Password</a> </h3><br/>
+							</Card.Text> 
+							</form>
+						</Card.Body> 
+					</Card> 
+				</Col>
+			</Row>
+		</Container>
     )
 }
